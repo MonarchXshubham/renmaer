6cf0fb39e8842b4a4403425101ecaa173e48b1fc
MonarchXshubham <shubhamxmonarch@gmail.com>
2024-07-24 13:09:51 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,106 @@
+import os, time, re
+id_pattern = re.compile(r'^.\d+$')
+
+
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "28243586")
+    API_HASH  = os.environ.get("API_HASH", "4022d5686b9b7a7cf8891205921a0ab3")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7391583970:AAHEmCU2fighBM8ILE4VNFUbUTmvdnekLKY") 
+   
+    # database config
+    DB_NAME = os.environ.get("DB_NAME","cluster0")     
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://smkbotz:hDNia3OPBZ9iIwxc@cluster0.dbsoeis.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ 
+    # other configs
+    BOT_UPTIME  = time.time()
+    START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/f2a5fd52b6edf6976c8f1.jpg")
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
+
+    # channels logs
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "-1002235661888") 
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002235766670"))
+
+    # wes response configuration     
+    WEBHOOK = bool(os.environ.get("WEBHOOK", True))
+
+
+
+class Txt(object):
+    # part of text configuration
+    START_TXT = """Hello {} 👋 
+
+➻ This Is An Advanced And Yet Powerful Rename Bot.
+
+➻ Using This Bot You Can Rename And Change Thumbnail Of Your Files.
+
+➻ You Can Also Convert Video To File And File To Video.
+
+➻ This Bot Also Supports Custom Thumbnail And Custom Caption.
+
+<b>Bot Is Made By :</b> @Unreal_X_Bot"""
+
+    ABOUT_TXT = """
+╭───────────────⍟
+├<b>🤖 My Name</b> : {}
+├<b>🌌 Developer</b> : <a href=https://t.me/SHUBHAM_X_OFFICIAL>♡ ꜱʜᴜʙʜᴀᴍ ♡</a> 
+├<b>🖥️ ᴄʜᴀɴɴᴇʟ</b> : <a href=https://t.me/Unreal_X_Bot>ᴜɴʀᴇᴀʟ ʙᴏᴛ</a> 
+├<b>👨‍💻 ɢʀᴏᴜᴘ</b> : <a href=https://t.me/Unreal_X_Support>ᴜɴʀᴇᴀʟ ꜱᴜᴘᴘᴏʀᴛ</a>
+├<b>📕 Library</b> : <a href=https://github.com/pyrogram>Pyrogram</a>
+├<b>✏️ Language</b> : <a href=https://www.python.org>Python 3</a>
+├<b>💾 Database</b> : <a href=https://cloud.mongodb.com>Mongo DB</a>  
+╰───────────────⍟
+"""
+
+    HELP_TXT = """
+🌌 <b><u>How To Set Thumbnail</u></b>
+  
+➪ /start - Start The Bot And Send Any Photo To Automatically Set Thumbnail.
+➪ /del_thumb - Use This Command To Delete Your Old Thumbnail.
+➪ /view_thumb - Use This Command To View Your Current Thumbnail.
+
+📑 <b><u>How To Set Custom Caption</u></b>
+
+➪ /set_caption - Use This Command To Set A Custom Caption
+➪ /see_caption - Use This Command To View Your Custom Caption
+➪ /del_caption - Use This Command To Delete Your Custom Caption
+➪ Example - <code>/set_caption 📕 Name ➠ : {filename}
+
+🔗 Size ➠ : {filesize} 
+
+⏰ Duration ➠ : {duration}</code>
+
+✏️ <b><u>How To Rename A File</u></b>
+
+➪ Send Any File And Type New File Name And Select The Format [ Document, Video, Audio ].           
+
+𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/unreal_x_bot/>Developer</a>
+"""
+
+    PROGRESS_BAR = """\n
+ <b>🔗 Size :</b> {1} | {2}
+️ <b>⏳️ Done :</b> {0}%
+ <b>🚀 Speed :</b> {3}/s
+️ <b>⏰️ ETA :</b> {4}
+"""
+
+    DONATE_TXT = """
+<b>🥲 Thanks For Showing Interest In Donation! ❤️</b>
+
+If You Like My Bots & Projects, You Can 🎁 Donate Me Any Amount From 10 Rs Upto Your Choice.
+
+<b>🛍 UPI ID:</b> `Maurya-Shubham@fam`
+"""
+
+
+
+
+
+
+
+
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @JishuBotz
+# Developer @JishuDeveloper
